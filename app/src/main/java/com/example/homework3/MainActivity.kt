7734fca3722337abaf271b816ec6d7e0880cd958
lmelikishvili <levanimelikishvili@gmail.com>
2023-10-27 18:08:38 +0400

    form1

---
+++ app/src/main/java/com/example/homework3/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.homework3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        this.title = "My Title";
+
+
+    }
+}
